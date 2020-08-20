ff9c30423f046f70ee2b997b6912493b6de915a0
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-20 18:16:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,10 @@ def load_library(file_path)
   emoticons = YALM.load_file (file_path)
   new_hash = {}
   # code goes here
+  emoticons.each do |key, value|
+    new_hash [key] = {}
+    new_hash [key][:english] = value [0]
+    new_hash [key][japanese] = value [1]
 end
 new_hash
 end
