74091a225ae57c2a46201529724913350d12726c
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-20 18:22:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
   emoticons.each do |key, value|
     new_hash [key] = {}
     new_hash [key][:english] = value [0]
-    new_hash [key][japanese] = value [1]
+    new_hash [key][:japanese] = value [1]
 end
 new_hash
 end
